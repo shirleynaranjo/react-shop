8ed99827d64a17d98120fb0811ed76e4cb31c384
Shirley Naranjo <shirleypaolanv@ufps.edu.co>
2021-12-09 10:16:56 -0500

    componentes y rutas de React Shop

---
+++ src/containers/MyOrder.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import OrderItem from '../components/OrdenItem'
+
+const MyOrder = () => {
+    return (
+        <aside className="MyOrder">
+            <div className="title-container">
+                <img src="./icons/flechita.svg" alt="arrow"/>
+                <p className="title">My Order</p>
+            </div>
+            <div className="my-order-content">
+                <OrderItem/>
+                <div className="order">
+                    <p>
+                        <span>Total</span>
+                    </p>
+                    <p>$560.00</p>
+                </div>
+                <button className="primary-button">
+                    Checkout
+                </button>
+            </div>
+        </aside>
+    );
+}
+
+export default MyOrder;
